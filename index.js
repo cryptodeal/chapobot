c3aab3a981b78fefc2298ee2f3f3cac980125f7a
cryptodeal <jimmydeal16@gmail.com>
2021-02-27 20:56:07 -0600

    optimize user ws lifecycle timer

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const Sockette = require('sockette');
 const {initSocket} = require('./utils/socket');
 const {constructComment} = require('./utils/opHelpers');
 const host = 'www.chapo.chat';
-const wsEndpoint = 'api/v1/ws'
+const wsEndpoint = 'api/v1/ws';
+const userSocketInstances = [];
+
 //declare some temp constants for testing purposes
 const user = {
   jwt: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6NjQxOCwidG9rZW5faWQiOiI1OTUzMWViNy0zODcyLTQyN2UtYTBlNC03OTFhYjY4NGI4ZTIiLCJpc3MiOiJ3d3cuY2hhcG8uY2hhdCJ9.MrrzeWZ-x3fgo6nbD0koVKLzkW_o5Ww9peOLzkA4RTY",
@@ -12,15 +14,22 @@ const user = {
 const post = {
   id: 88066
 }
-const commentBody = "bsdfg"
+const commentBody = "asdlkfj"
 
 
-const userLifeCycle = (user, commentBody, post) => {
+const userLifeCycle = (user, commentBody, post, next) => {
   const userSocket = initSocket();
+  let count = 0;
   setInterval(() => {
     if(userSocket.ws){
-      let commentPayload = constructComment(user, commentBody, post)
-      userSocket.json(commentPayload)
+      while(count < 1){
+        let commentPayload = constructComment(user, commentBody, post)
+        userSocket.json(commentPayload)
+        count++;
+      }
+      console.log(`done!!!`)
+    } else {
+      console.log(`UserID: ${user.id} opening connection`)
     }
   }, 1000);
 }
--- utils/opHelpers.js
+++ utils/opHelpers.js
@@ -10,3 +10,68 @@ exports.constructComment = (user, commentBody, post) => {
   } 
   return commentPayload
 }
+
+exports.constructPost = (user, post) => {
+  let commentPayload = {
+    op: "CreatePost",
+    data: {
+      auth: user.jwt,
+      content: commentBody,
+      creator_id: user.id, 
+      post_id: post.id
+    }
+  } 
+  return commentPayload
+}
+exports.constructPostLike = (user, post, isLike) => {
+  //isLike is boolean: true => add like to the post || false => remove like from the post
+  if(isLike === true){
+    let postLikePayload = {
+      op: "CreatePostLike",
+      data: {
+        auth: user.jwt,
+        post_id: post.id,
+        score: 1
+      }
+    }
+    return postLikePayload
+  } else{
+    let postLikePayload = {
+      op: "CreatePostLike",
+      data: {
+        auth: user.jwt,
+        post_id: post.id,
+        score: 0
+      }
+    }
+    return postLikePayload
+  }
+}
+
+
+exports.constructCommentLike = (user, post, comment, isLike) => {
+  //isLike is boolean: true => add like to the post || false => remove like from the post
+  if(isLike === true){
+    let commentLikePayload = {
+      op: "CreateCommentLike",
+      data: {
+        auth: user.jwt,
+        comment_id: comment.id,
+        post_id: post.id,
+        score: 1
+      }
+    }
+    return commentLikePayload
+  } else{
+    let commentLikePayload = {
+      op: "CreateCommentLike",
+      data: {
+        auth: user.jwt,
+        comment_id: comment.id,
+        post_id: post.id,
+        score: 0
+      }
+    }
+    return commentLikePayload
+  }
+}
